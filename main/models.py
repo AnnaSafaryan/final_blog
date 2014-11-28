3d87a2e27fa6664bf7211e9a63afe4676471c57f
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-11-28 16:43:39 +0300

    Adding 'published' field to Post models

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     header = models.CharField(max_length=255)
     text = models.TextField()
     when = models.DateTimeField(auto_now=True)
+    publish = models.BooleanField(default=True)
 
     def __unicode__(self):
         return u'{} at {}'.format(self.header, self.when)
